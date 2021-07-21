104e4e4e3b75e83c67fa848eef5c56dced7ef017
Sarfaraz <m.s.hussain@outlook.com>
2021-07-21 18:13:47 -0500

    added connection and inital inquirer questions

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employeeDB"
+  });
+
+  function inquire() { [
+    inquirer.prompt({
+      type: "list",
+      message: "What do you need to do?",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View All Employee Roles",
+        "View All Employee Departments",
+        "Edit Employee Info",
+        "Add an Employee",
+        "Add a Role",
+        "Add a Department"
+      ]   
+    })
+  ]}
+
+  //sql queries to post, read and delete data
+  
