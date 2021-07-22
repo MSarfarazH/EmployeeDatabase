9cdf8bb2af7ee565f94d47c7fc786107a46b6b09
Sarfaraz <m.s.hussain@outlook.com>
2021-07-22 18:14:41 -0500

    added schema and seed file

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+
+USE employeedb;
+
+INSERT INTO department (name)
+VALUES 
+('Engineers'),
+('RnD'),
+('Board'),
+('Finance'),
+('Marketing'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Dev Ops Engineer', 100000, 1),
+('Operations Analyist', 120000, 2),
+('CEO', 200000, 3),
+('Accountant', 100000, 4),
+('Marketer', 90000, 5),
+('Sales Rep', 40000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Mohammad', 'Hussain', 1, 653),
+('Joe', 'Biden', 2, 666),
+('Xi', 'JinPing', 3, 234),
+('Valdimir', 'Putin', 4, 734),
+('Imran', 'Khan', 5, 231),
+('Recep', 'Erdogan', 6, 987);
